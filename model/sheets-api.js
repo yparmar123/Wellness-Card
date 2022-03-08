8df35c48e3abcea8967e8e25a88584ce38f1452f
yparmar2080 <61891116+yparmar123@users.noreply.github.com>
2022-03-08 17:29:24 -0500

    login page added, css for buttons added, created AddDiary and Overwrite route, changed root route to be a welcome page, added login page, added a generateSheetRange function, added edited write diary function to make a new page if none exists or write in the correct row. Created function to copy layout page and paste it into a newly created page. Need to fix bug where when newly created page is made, the diary entry does not get entered correctly.

--- model/sheets-api.js
+++ model/sheets-api.js
@@ -28,57 +28,50 @@ module.exports.initialize = async () => {
       console.log(err);
     } else {
       console.log("Connection Successful!");
-      let exists;
-      this.checkDiary(client).then((data) => {
-        exists = data;
-        console.log(exists);
-      });
+      // createNewSheet()
+      //   .then()
+      //   .catch((err) => console.log(err));
+      this.writeDiaryDay();
     }
   });
 };
 
-module.exports.checkDiary = async (cl) => {
-  const gsapi = google.sheets({ version: "v4", auth: cl });
-  const currentDate = new Date();
+module.exports.checkDiary = async () => {
+  // Creates api variable
+  const gsapi = google.sheets({ version: "v4", auth: client });
+
+  // Return value of function
   let exists = false;
-  let currentDay =
-    currentDate.getDay() < 2 ? currentDate.getDay() + 6 : currentDate.getDay();
 
-  let firstDayDifference = currentDay - 2;
+  range;
 
-  let firstDate = new Date();
-  firstDate.setDate(firstDate.getDate() - firstDayDifference);
-  let lastDate = new Date();
-  lastDate.setDate(firstDate.getDate() + 6);
-
+  // These options are the ones used to check if a diary card exists for the current day.
+  // We fill up the data using date information gathered from the previous code.
   const opt = {
-    spreadsheetId: "1u9R6fHRkt2NxIsbScEvWYcbozIi6pjuhiHmiCQ9dtXQ",
-    range: `${months[firstDate.getMonth()]}. ${firstDate.getDate()} - ${
-      months[lastDate.getMonth()]
-    }. ${lastDate.getDate()}, ${lastDate.getFullYear()}!B${currentDay + 1}:W${
-      currentDay + 1
-    }`,
+    spreadsheetId: process.env.sheet_id,
+    range: generateSheetRange(),
   };
 
   gsapi.spreadsheets.create();
+
+  // Basically if no sheet for that week exists, or no data is filled up for that day in the sheet,
+  // exists remains false. In the case that there are values in the correct sheet, exists becomes
+  // true
   try {
     let data = await gsapi.spreadsheets.values.get(opt);
     if (data.data.values) {
-      console.log("diary does exist");
       exists = true;
-    } else {
-      console.log("diary does not exist");
     }
-  } catch (err) {
-    console.log("diary does not exist");
-  }
+  } catch (err) {}
+
+  // Exists is returned
   return exists;
 };
 
-module.exports.getDiary = async (cl) => {
-  const gsapi = google.sheets({ version: "v4", auth: cl });
+module.exports.getDiary = async () => {
+  const gsapi = google.sheets({ version: "v4", auth: client });
   const opt = {
-    spreadsheetId: "1u9R6fHRkt2NxIsbScEvWYcbozIi6pjuhiHmiCQ9dtXQ",
+    spreadsheetId: process.env.sheet_id,
     range: "Jan. 18 - Jan. 24, 2022!C1:D1",
   };
 
@@ -87,29 +80,151 @@ module.exports.getDiary = async (cl) => {
   console.log(data.data.values);
 };
 
-module.exports.writeDiaryDay = async (cl) => {
-  const gsapi = google.sheets({ version: "v4", auth: cl });
-  const currentDate = new Date();
-  let currentDay =
-    currentDate.getDay() < 2 ? currentDate.getDay() + 7 : currentDate.getDay();
-
-  let firstDayDifference = currentDay - 2;
-
-  let firstDate = new Date();
-  firstDate.setDate(firstDate.getDate() - firstDayDifference);
-  let lastDate = new Date();
-  lastDate.setDate(firstDate.getDate() + 6);
-
+module.exports.writeDiaryDay = async () => {
+  const gsapi = google.sheets({ version: "v4", auth: client });
   const opt = {
-    spreadsheetId: "1u9R6fHRkt2NxIsbScEvWYcbozIi6pjuhiHmiCQ9dtXQ",
-    range: `${months[firstDate.getMonth()]}. ${firstDate.getDate()} - ${
-      months[lastDate.getMonth()]
-    }. ${lastDate.getDate()}, ${lastDate.getFullYear()}!B${currentDay + 1}:W${
-      currentDay + 1
-    }`,
+    spreadsheetId: process.env.sheet_id,
+    range: generateSheetRange(),
     valueInputOption: "USER_ENTERED",
     resource: { values: [["1", "2", "3", "HI"]] },
   };
 
-  let res = await gsapi.spreadsheets.values.update(opt);
+  gsapi.spreadsheets.values
+    .update(opt)
+    .then()
+    .catch((err) => {
+      createNewSheet()
+        .then(() => {
+          gsapi.spreadsheets.values
+            .update(opt)
+            .then()
+            .catch((err) => {
+              console.log("second values failed");
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+};
+
+// This function retrieves diary questions
+// from the user's sheets settings page
+
+module.exports.retrieveDiaryQuestions = async () => {
+  // initiates connection with sheets
+  const gsapi = google.sheets({ version: "v4", auth: client });
+  gsapi.spreadsheets.create();
+
+  // retrieves number of elements per table
+  const opt = {
+    spreadsheetId: process.env.sheet_id,
+    range: "Settings!A3:A4",
+  };
+  let tableData = await gsapi.spreadsheets.values.get(opt);
+
+  // retrieves first table questions
+  opt.range = `Settings!B3:B${tableData.data.values[0][0] + 2}`;
+  let tableQuestions = await gsapi.spreadsheets.values.get(opt);
+
+  // retrieves second table questions
+  opt.range = `Settings!C3:C${tableData.data.values[1][0] + 2}`;
+  let secondTableQuestsions = await gsapi.spreadsheets.values.get(opt);
+
+  // variable stores both table's questions in an object
+  let questions = {
+    firstTable: tableQuestions.data.values.map((el) => el[0]),
+    secondTable: secondTableQuestsions.data.values.map((el) => el[0]),
+  };
+
+  return questions;
+};
+
+// This function creates a new page and copies the layout from the layout page
+// in the spreadsheet
+let createNewSheet = async () => {
+  // initiates connection with sheets
+  const gsapi = google.sheets({ version: "v4", auth: client });
+
+  // this section copies the layout for the new sheet being created
+  const opt = {
+    spreadsheetId: process.env.sheet_id,
+    range: "Layout!A1:W19",
+  };
+  let tableData = await gsapi.spreadsheets.values.get(opt);
+
+  let layout = tableData.data.values;
+
+  // Title is generated for the new sheet being created.
+  // This is made by generating the whole range of a sheet while substringing from "!"
+  let sheetTitle = generateSheetRange();
+  sheetTitle = sheetTitle.substring(0, sheetTitle.indexOf("!"));
+
+  // Request properties are set here
+  const requests = [
+    {
+      addSheet: {
+        properties: {
+          title: sheetTitle,
+        },
+      },
+    },
+  ];
+
+  // This function is used to add a new sheet
+  await gsapi.spreadsheets.batchUpdate(
+    {
+      spreadsheetId: process.env.sheet_id,
+      resource: { requests },
+    },
+    (err, res) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      } else {
+        // If new sheet is created, the text is copied from the layout data to the new sheet.
+        // Options are updated to update the new sheet.
+        opt.range = sheetTitle + "!A1:W19";
+        opt.resource = { values: layout };
+        opt.valueInputOption = "USER_ENTERED";
+
+        gsapi.spreadsheets.values
+          .update(opt)
+          .then()
+          .catch((err) => console.log(err));
+      }
+      return res;
+    }
+  );
+};
+
+// This function generates a string which represents the range of today's
+// date in terms of which cells to edit and which sheet to be on.
+const generateSheetRange = () => {
+  // Variable saves current date
+  const currentDate = new Date();
+
+  // Since the diary card we are working with starts on Tuesdays,
+  // this line of code checks if the day is before Tuesday. If so
+  // the current day will have 7 added to it to make the day work
+  // with our sheets layout.
+  let currentDay =
+    currentDate.getDay() < 2 ? currentDate.getDay() + 7 : currentDate.getDay();
+
+  // Subtracting 2 allows for us to view the week from 1-7 but with Tuesday as the first day
+  currentDay -= 2;
+
+  // Sets the first date of the week to the Tuesday of the week
+  let firstDate = new Date();
+  firstDate.setDate(firstDate.getDate() - currentDay);
+
+  // Sets the last date of the week to the Monday of the week
+  let lastDate = new Date();
+  lastDate.setDate(firstDate.getDate() + 6);
+
+  return `${months[firstDate.getMonth()]}. ${firstDate.getDate()} - ${
+    months[lastDate.getMonth()]
+  }. ${lastDate.getDate()}, ${lastDate.getFullYear()}!B${currentDay + 3}:W${
+    currentDay + 3
+  }`;
 };
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,41 @@ const sheetsApi = require("../model/sheets-api");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   sheetsApi.initialize();
-  res.render("index", { title: "DBT Diary Card" });
+  res.render("index", { title: "Wellness Card - Home Page" });
 });
 
-router.get("/diary", function (req, res, next) {
-  res.render("diary", { title: "Diary" });
+router.get("/AddDiary", function (req, res, next) {
+  sheetsApi
+    .initialize()
+    .then(() => {
+      sheetsApi
+        .checkDiary()
+        .then((exists) => {
+          if (exists) {
+            res.redirect("/Overwrite");
+          } else {
+            sheetsApi.retrieveDiaryQuestions().then((data) => {
+              console.log(exists);
+              res.render("diary", {
+                title: "Wellness Card - Add Diary",
+                questions: data,
+              });
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/Overwrite", function (req, res, next) {
+  res.render("overwrite", {
+    title: "Wellness Card - Overwrite",
+  });
 });
 
 module.exports = router;
