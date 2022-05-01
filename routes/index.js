27ebbb9f39ad02e5210f810fee93c61d9b500a67
yparmar2080 <61891116+yparmar123@users.noreply.github.com>
2022-05-01 16:53:19 -0400

    bug fixed where error would appear when writing to newly created sheet, more comments have been added as well

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,27 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Wellness Card - Home Page" });
 });
 
+// Add Diary page
 router.get("/AddDiary", function (req, res, next) {
   sheetsApi
     .initialize()
     .then(() => {
+      // First a check is done to see if diary already exists, this allows us
+      // to know if an overwrite popup should appear or not
       sheetsApi
         .checkDiary()
         .then((exists) => {
+          // Diary questions are retrieved with this call, data is an array of
+          // diary questions
           sheetsApi.retrieveDiaryQuestions().then((data) => {
             let overwrite = false;
-
+            // If "exist" value is the string exists, that means a diary exists.
+            // Overwrite is set to true, meaning an overwrite modal will pop up when page loads
             if (exists === "exists") {
               overwrite = true;
             }
 
+            // Diary view is rendered, the questions and overwrite value are passed to the front end
             res.render("diary", {
               title: "Wellness Card - Add Diary",
               questions: data,
@@ -38,6 +45,9 @@ router.get("/AddDiary", function (req, res, next) {
     });
 });
 
+// This post handles submission of diary entries. It takes in values of the first
+// and second table, then passes it over to the writeDiaryDay function. From there it
+// is sent over to the sheet assigned to the account owner
 router.post("/SubmitDiary", function (req, res, next) {
   sheetsApi.initialize().then(() => {
     sheetsApi
